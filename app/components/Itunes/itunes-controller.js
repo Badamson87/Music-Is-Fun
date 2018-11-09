5fea017463eb4327287aed97ef805c9650c68ead
Brian Adamson <brianadamson87@gmail.com>
2018-11-09 16:09:24 -0700

    Position fixed working

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -14,12 +14,23 @@ function drawSongs(results) {
   for (let i = 0; i < results.length; i++) {
     const songs = results[i];
   } results.forEach(Song => {
-    template += `<p>${Song.title}</p>`
+    template += `
+   <div class="card" style="width: 12rem;">
+  <img class="card-img-top" src="${Song.albumArt}" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="${Song.artist}">${Song.artist}</h5>
+    <p class="${Song.title}">${Song.title}</p>
+    <p class="${Song.price}">${Song.price}</p>
+    <a href="${Song.previewUrl}" class="btn btn-primary">Preview</a>
+  </div>
+</div>
+    `
   });
 
   document.getElementById("songsList").innerHTML = template
 }
 
+{/* <p>${Song.title}</p> */ }
 
 //PUBLIC
 class ItunesController {
